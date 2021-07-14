722bff1f0074d826846088be2d458b062b8ad8ce
TimZoe <contact@trophie.de>
2021-07-14 14:01:56 +0200

    ohneLogIn

--- src/App.js
+++ src/App.js
@@ -1,16 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
-import Amplify, { Auth } from 'aws-amplify';
-import awsconfig from './aws-exports';
-import { withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react';
 
-Amplify.configure(awsconfig);
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <AmplifySignOut/>
+       
 
         <p>
           Trophie 14.07.
@@ -22,4 +18,4 @@ function App() {
   );
 }
 
-export default withAuthenticator(App);
+export default App;
