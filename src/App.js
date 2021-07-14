a75da22d6d051d9042519e02bdae4a9894bec85e
TimZoe <contact@trophie.de>
2021-07-14 13:39:18 +0200

    logIn

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
+import Amplify from 'aws-amplify';
+import awsconfig from './aws-exports';
+import {AmplifySignOut, withAuthenticator} from '@aws-amplify/ui-react';
+
+Amplify.configure(awsconfig)
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <AmplifySignOut/>
+
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Trophie 14.07.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      
+         
       </header>
     </div>
   );
 }
 
-export default App;
+export default withAuthenticator(App);
